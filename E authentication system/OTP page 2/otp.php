ac813c25ddb242190851be39dc0083055d8b7815
Masteride001740 <rashikabdul87@gmail.com>
2025-03-26 02:18:51 -0700

    Update otp.php

--- E authentication system/OTP page 2/otp.php
+++ E authentication system/OTP page 2/otp.php
@@ -7,24 +7,22 @@
     <title>Verify Account</title>
   </head>
   <body>
-    <form method=post action='<B>sendsms.php</B>'>
-    <div class="container">
-      <h2>Verify Your OTP</h2>
-      <p>We messaged you the six digit code  <br/> Enter the code below to confirm your identity.</p>
-      <div class="code-container">
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
-        <input type="number" class="code" placeholder="0" min="0" max="9" required>
+    <form method="post" action="sendsms.php">
+      <div class="container">
+        <h2>Verify Your OTP</h2>
+        <p>We messaged you the six-digit code <br /> Enter the code below to confirm your identity.</p>
+        <div class="code-container">
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+          <input type="text" class="code" placeholder="0" inputmode="numeric" maxlength="1" required />
+        </div>
+        <!-- Submit Button -->
+        <button type="submit" class="button" style="margin-left:120px; margin-right: 120px;">Verify</button>
       </div>
-      
-        <a href="../project website/demo 3/index.html" class="button" style="margin-left:120px; margin-right: 120px; text-decoration: none;">Verify</a>
-      
-      
-    </div>
-    <script src="script.js"></script>
     </form>
+    <script src="script.js"></script>
   </body>
 </html>
